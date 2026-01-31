4274aec327d4b35e6597ada237ed4336079e0b39
shirleyperew <shirleyperew@gmail.com>
2026-01-31 15:34:02 +0800

    Update BasicMachineProblem.java

--- BasicMachineProblem.java
+++ BasicMachineProblem.java
@@ -12,12 +12,10 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasicMachineProblem {
-    
-        WebDriver driver;
+    WebDriver driver;
 
     @BeforeMethod
     public void setup() {
@@ -32,44 +30,44 @@ public class BasicMachineProblem {
         driver.get("https://the-internet.herokuapp.com");
         Thread.sleep(2000);
 
-        //1. get page title
+        //1. Get the page title
         String pageTitle = driver.getTitle();
         System.err.println("Page Title: "+ pageTitle);
         Assert.assertEquals(pageTitle,"The Internet");
 
 
-        //2. interaction and locators
+        //2. For Interaction and Locators
 
-        //a. locate Form Authentication Link
+        //a. Locate Form Authentication Link
         driver.findElement(By.linkText("Form Authentication")).click();
         Thread.sleep(2000);
         
-        //b. locate username input field
+        //b. Locate username input field
         WebElement usernameInput = driver.findElement(By.name("username"));
         usernameInput.sendKeys("tomsmith");
 
-        //c. locate password input field
+        //c. Locate password input field
         WebElement passwordInput = driver.findElement(By.name("password"));
         passwordInput.sendKeys("SuperSecretPassword!");
         Thread.sleep(2000);
 
-        //d. locate login button
+        //d. Locate login button
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
         Thread.sleep(2000);
     
-        //e. get result - login successful
+        //e. Get result for login successful
         WebElement successMessage = driver.findElement(By.id("flash"));
         Assert.assertTrue(successMessage.isDisplayed()," You logged into a secure area!");
         System.out.println("Login Success Message: 'You logged into a secure area!' is displayed.");
         Thread.sleep(2000);
 
-        //f. locate logout button
+        //f. Locate logout button
         WebElement logoutButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/a"));
         logoutButton.click();
         Thread.sleep(5000);
 
-        //g. go back to main page
+        //g. Back to Main Page
         driver.navigate().to("https://the-internet.herokuapp.com");
         Thread.sleep(2000);
 
@@ -79,28 +77,28 @@ public class BasicMachineProblem {
         driver.findElement(By.linkText("Dropdown")).click();
         Thread.sleep(2000);        
 
-        //b. option 2 dropdown
-        //locate dropdown element
+        //b. For option 2 dropdown
+        //Locate dropdown element
         WebElement dropdownElement = driver.findElement(By.id("dropdown"));
 
-        //create select object
+        //Create select object
         Select dropdown = new Select(dropdownElement);
         dropdown.selectByValue("2");
         Assert.assertEquals(dropdown.getFirstSelectedOption().getText(), "Option 2"); 
         System.out.println("Dropdown Selection: Option 2 Dropdown is selected as expected.");
         Thread.sleep(2000);
         
-        //c. go back to main page
+        //c. Back to Main Page
         driver.navigate().back();
         Thread.sleep(2000);
 
 
         //4. Checkboxes
-         //a. locate Checkboxes Link
+         //a. Locate Checkboxes Link
         driver.findElement(By.linkText("Checkboxes")).click();
         Thread.sleep(2000);
 
-        //b. check if checkbox 1 is checked
+        //b. Check if checkbox 1 is checked
         WebElement checkbox1 = driver.findElement(By.xpath("//input[@type='checkbox'][1]"));
         Assert.assertFalse(checkbox1.isSelected(), "Checkbox 1 should be unselected by default");
         checkbox1.click();
@@ -108,13 +106,13 @@ public class BasicMachineProblem {
         Assert.assertTrue(checkbox1.isSelected(), "Checkbox 1 should be selected after clicking");
         System.out.println("Checkbox Selection: Checkbox 1 is selected as expected.");
 
-        //c. check if checkbox 2 is selected
+        //c. Check if checkbox 2 is selected
         WebElement checkbox2 = driver.findElement(By.xpath("//input[@type='checkbox'][2]"));
         Assert.assertTrue(checkbox2.isSelected(), "Checkbox 2 should be selected by default");
         System.out.println("Checkbox Selection: Checkbox 2 is selected as expected.");
         Thread.sleep(2000); 
 
-        //d. go back to main page
+        //d. Back to Main Page
         driver.navigate().back();
         Thread.sleep(2000);
 
@@ -124,7 +122,7 @@ public class BasicMachineProblem {
         driver.findElement(By.linkText("JavaScript Alerts")).click();
         Thread.sleep(2000);         
 
-        //b. click JS Alert button
+        //b. Click JS Alert button
         WebElement jsalertButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/ul/li[1]/button"));
         jsalertButton.click();
         Thread.sleep(2000);
@@ -139,13 +137,13 @@ public class BasicMachineProblem {
         System.out.println("Alert Success Message: 'You successfully clicked an alert' is displayed.");
         Thread.sleep(2000);
    
-        //e. go back to main page
+        //e. Back to Main Page
         driver.navigate().back();
         Thread.sleep(2000);
 
 
         //6. Explicit waits
-         //a. locate Dynamic Loading Link
+         //a. Locate Dynamic Loading Link
         driver.findElement(By.linkText("Dynamic Loading")).click();
         Thread.sleep(2000);          
 
@@ -178,5 +176,6 @@ public class BasicMachineProblem {
                 driver.quit();
         }
     }
+
 
 }
